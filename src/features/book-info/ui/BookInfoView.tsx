07df3289167ace07e6e8f52a2834f482c66f0b55
psh9321 <parksuhyun9321@gmail.com>
2026-04-08 00:15:08 +0900

    도서 상세 페이지 ui 수정
     - max-width : 500 미만 일 시 px 수정

--- src/features/book-info/ui/BookInfoView.tsx
+++ src/features/book-info/ui/BookInfoView.tsx
@@ -43,7 +43,7 @@ export const BookInfoView = ({ status } : BOOK_INFO_VIEW) => {
         <article className="relative bg-[#2A2F32] rounded-[10px] select-none [@media(max-width:499px)]:w-[calc(100%-40px)]">
             <h2 className="sr-only">{data?.["title"]} 도서 정보</h2>
             <button onClick={NaviCallback} className="absolute top-[-40px] left-[0]" title="뒤로가기"><X size={35}/></button>
-            <div className="flex flex-wrap justify-between w-[450px] h-[530px] mx-auto py-[40px] px-[40px] overflow-y-auto [@media(max-width:499px)]:block [@media(max-width:499px)]:w-full [@media(max-width:499px)]:px-0 [@media(max-width:499px)]:text-center">
+            <div className="flex flex-wrap justify-between w-[450px] h-[530px] mx-auto py-[40px] px-[40px] overflow-y-auto [@media(max-width:499px)]:block [@media(max-width:499px)]:w-full [@media(max-width:499px)]:px-[10px] [@media(max-width:499px)]:text-center">
                 <div className="relative w-[150px] [@media(max-width:499px)]:inline-flex [@media(max-width:499px)]:flex-col">
                     <Image className="inline-block mb-[15px]" loading="eager" width={150} height={220} sizes="100vw" src={data?.["cover"]??""} alt={`${data?.["title"]} 커버 이미지`} />
                     <BtnRegister item={data} status={status} />
@@ -61,7 +61,7 @@ export const BookInfoView = ({ status } : BOOK_INFO_VIEW) => {
                 </dl>
                 <dl className="w-full leading-[1.8] mt-[50px] break-keep">
                     <dt className="inline-block mb-[10px] border-b">책 소개</dt>
-                    <dd className="[@media(max-width:499px)]:px-[40px] [@media(max-width:499px)]:text-left">{data?.["description"]}</dd>
+                    <dd className="[@media(max-width:499px)]:px-[20px] [@media(max-width:499px)]:text-left">{data?.["description"]}</dd>
                 </dl>
             </div>
         </article>
