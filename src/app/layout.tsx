2c270209321935a47aec6f7250b4e918a074c308
psh9321 <parksuhyun9321@gmail.com>
2026-04-26 05:40:38 +0900

    메타 데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,18 +6,18 @@ import SessionProvider from "@/providers/SessionProvider";
 import { LoadingView } from "@/shared/ui/loadingView";
 
 export const metadata: Metadata = {
-    metadataBase: new URL("https://next-booknote.vercel.app"),
+    metadataBase: new URL("https://booknote.psh9321-portfolio.p-e.kr"),
     creator : "프론트엔드 개발자 박수현",
     publisher : "프론트엔드 개발자 박수현",
     title: {
-        default: "북로그",
+        default: "북노트",
         template: "%s | 독서기록 플랫폼",
     },
     description: "전국 전시 정보를 한눈에 확인하세요",
     keywords : ["독서", "읽고싶은", "읽는중", "완독 도서", "book", "book log", "book log"],
     appLinks : {
         web : {
-        url : "https://next-booknote.vercel.app",
+        url : "https://booknote.psh9321-portfolio.p-e.kr",
         should_fallback : true,
         }
     },
